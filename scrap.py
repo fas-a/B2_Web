9852f1117e29d653aa32af48b9fc9241e3559d67
iamPanda4 <abulkhoirfaris0@gmail.com>
2023-02-25 23:17:51 +0700

    add time scraping

--- scrap.py
+++ scrap.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 from bs4 import BeautifulSoup
 URL = "https://bola.kompas.com/"
 page = requests.get(URL)
@@ -13,14 +14,17 @@ kategori1 = latest[0].find_all("h2", class_="article__subtitle")
 kategori2 = latest[1].find_all("h2", class_="article__subtitle")
 result = []
 for i in range(len(title1) + len(title2)):
+    current_time = time.ctime()
     if i < len(title1) :
         result.append({"id":i+1,"judul":title1[i].text.strip(),
                    "waktu":date1[i].text.strip()
-                       , "kategori":kategori1[i].text.strip()})
+                       , "kategori":kategori1[i].text.strip(),
+                       "waktu_scraping":current_time})
     else :
         result.append({"id":i+1,"judul":title2[i- len(title1)].text.strip(),
                    "waktu":date2[i- len(title1)].text.strip()
-                       , "kategori":kategori1[i - len(title1)].text.strip()})
+                       , "kategori":kategori1[i - len(title1)].text.strip(),
+                       "waktu_scraping":current_time})
 HasilJSON = json.dumps(result)
 JSONFile = open("BeritaTerkini.json","w")
 JSONFile.write(HasilJSON)
